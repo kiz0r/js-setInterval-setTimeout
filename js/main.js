1456ba6302b1cab69c5c67b8557d567c3161e844
Kirill Khomenko <kkhomenko10@gmail.com>
2023-02-14 10:53:13 +0100

    finish all tasks

--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
 'use strict';
 
-// 2. *Виводити посилання через певний час після завантаження сторінки. Поки повідомлення не відображається, на його місці виводити зворотній відлік "Зачекайте хвилин:секунд".
-
 /* Task 1 */
 
 // printNumbers using setInterval
@@ -17,7 +15,7 @@ function printNumbers1(from, to, interval) {
   }, interval);
 }
 
-printNumbers1(5, 20, 2000);
+// printNumbers1(5, 20, 2000);
 
 // printNumbers using setTimeout with recursion
 function printNumbers2(from, to, interval) {
@@ -33,3 +31,25 @@ function printNumbers2(from, to, interval) {
 }
 
 // printNumbers2(5, 20, 1000);
+
+/* Task 2 */
+// 2. *Виводити посилання через певний час після завантаження сторінки. Поки повідомлення не відображається, на його місці виводити зворотній відлік "Зачекайте хвилин:секунд".
+
+const msgBox = document.querySelector('.msgBox');
+
+const link =
+  'https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg';
+
+function pushLink() {
+  let timeCount = 9000 * 0.001;
+  let intervalId = setInterval(() => {
+    msgBox.innerHTML = `Wait for ${timeCount} sec`;
+    if (timeCount === 0) {
+      msgBox.innerHTML = `<a href='${link}'>Cutie Cat ♥</a>`;
+      clearInterval(intervalId);
+    }
+    timeCount--;
+  }, 1000);
+}
+
+pushLink();
